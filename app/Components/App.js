b49d3ff009e067fe019705c0a76f773244ca5393
Simon Philouze <contact@simonphilouze.fr>
2019-11-08 13:32:01 +0100

    add directory viewer

--- app/Components/App.js
+++ app/Components/App.js
@@ -3,47 +3,65 @@ import React, { Fragment, useState } from 'react';
 const App = () => {
 
   const [contentFile, setContentFile] = useState("");
-  const [fileHandle, setFileHandle] = useState(null);
+  const [pathDirectory, setPathDirectory] = useState([]);
 
-  async function writeFile() {
-    // Create a writer (request permission if necessary).
-    const writer = await fileHandle.createWriter();
-    // Make sure we start with an empty file
-    await writer.truncate(0);
-    // Write the full length of the contents
-    await writer.write(0, contentFile);
-    // Close the file and write the contents to disk
-    await writer.close();
-  }
+  // async function writeFile() {
+  //   // Create a writer (request permission if necessary).
+  //   const writer = await fileHandle.createWriter();
+  //   // Make sure we start with an empty file
+  //   await writer.truncate(0);
+  //   // Write the full length of the contents
+  //   await writer.write(0, contentFile);
+  //   // Close the file and write the contents to disk
+  //   await writer.close();
+  // }
 
   const handleFileInput = async () => {
-    const fileHandle = await window.chooseFileSystemEntries();
-    const file = await fileHandle.getFile();
-    const content = await file.text();
+    const handle = await window.chooseFileSystemEntries({
+      type: 'openDirectory'
+    });
 
-    setFileHandle(fileHandle)
-    setContentFile(content);
+    const entries = await handle.getEntries();
+    let foo = [];
+
+    for await (const entry of entries) {
+      console.log(entry)
+      foo.push({
+        type: entry.isFile ? 'File' : 'Directory',
+        entry: entry,
+        name: entry.name
+      })
+    }
+    setPathDirectory(foo);
   }
 
   return (
     <Fragment>
-        <header>
-          <p>{fileHandle && fileHandle.name}</p>
-        </header>
         <div className="editor">
+          <div className="pathDirectory">
+            <div className="contentArbo">
+              {pathDirectory.length <= 0 ? <p>no data</p> : pathDirectory.map(item => {
+                return (
+                  <div className="section">
+                    <img className="iconPath" src={`${item.type === "File" ? "/assets/file.png" : "/assets/folder.png"}`} />
+                    <span className="nameItemPatchDirectory">{item.name}</span><br/>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
           <div className="input-file">
             {!contentFile ?
               <Fragment>
-                <p className="input-text">Click or Drag and Drop</p>
+                <p className="input-text">Click and Load</p>
                 <input type="file" onClick={handleFileInput}/>
               </Fragment>
             : <textarea value={contentFile} onChange={(event) => setContentFile(event.target.value)} />}
           </div>
         </div>
-        <footer>
-          <a className="link" target="_blank" href="https://github.com/HitAngry/React-NativeFileSystem">Github repo</a>
+        {/* <footer>
           <button className="save-button" onClick={writeFile} disabled={contentFile.length <= 0}>Save</button>
-        </footer>
+        </footer> */}
     </Fragment>
   )
 } 
